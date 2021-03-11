582db5c60cd160090377e0f9d756dbb0d527c09b
sibongiseni <sibongiseni.ndebele@idolconsulting.co.za>
2021-03-11 15:22:48 +0200

    updated

--- src/main/java/lany/gcode/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/lany/gcode/employeemanager/repo/EmployeeRepo.java
@@ -1,2 +1,9 @@
-package lany.gcode.employeemanager.repo;public interface EmployeeRepo {
+package lany.gcode.employeemanager.repo;
+
+import lany.gcode.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+    Employee findEmployeeById(Long id);
 }
--- src/main/java/lany/gcode/employeemanager/service/EmployeeService.java
+++ src/main/java/lany/gcode/employeemanager/service/EmployeeService.java
@@ -1,2 +1,40 @@
-package lany.gcode.employeemanager.service;public class EmployeeService {
+package lany.gcode.employeemanager.service;
+
+import lany.gcode.employeemanager.model.Employee;
+import lany.gcode.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployee(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id);
+    }
 }
